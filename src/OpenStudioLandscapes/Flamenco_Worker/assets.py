9adaee6361b57603fa0f27bdafe28b2ac23fc181
Michael Mussato <michimussato@gmail.com>
2025-11-17 14:07:13 +0100

    44-update-documentation: fix docker compose image paths

--- src/OpenStudioLandscapes/Flamenco_Worker/assets.py
+++ src/OpenStudioLandscapes/Flamenco_Worker/assets.py
@@ -497,7 +497,7 @@ def compose_flamenco_worker(
             "restart": "always",
             # "image": "${DOT_OVERRIDES_REGISTRY_NAMESPACE:-docker.io/openstudiolandscapes}/%s:%s"
             # % (build["image_name"], build["image_tags"][0]),
-            "image": "%s:%s" % (build["image_name"], build["image_tags"][0]),
+                "image": "%s%s:%s" % (build["image_prefixes"], build["image_name"], build["image_tags"][0]),
             "environment": {
                 "FLAMENCO_HOME": "/app/flamenco-worker-files",
                 "FLAMENCO_WORKER_NAME": host_name,
