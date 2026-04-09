38c56734f90149ec4a01b16904d4c795c088d331
Michael Mussato <michimussato@gmail.com>
2026-04-09 12:57:16 +0200

    - takes grafana for compose scope alloy into account. - adds common AssetSpec (group_out_base for now) to all Feature definitions.py

--- src/OpenStudioLandscapes/Flamenco_Worker/definitions.py
+++ src/OpenStudioLandscapes/Flamenco_Worker/definitions.py
@@ -4,6 +4,7 @@ from dagster import (
 )
 
 import OpenStudioLandscapes.Flamenco_Worker.assets
+from OpenStudioLandscapes.engine.features.upstream_asset_specs import assets_external
 
 assets = load_assets_from_modules(
     modules=[OpenStudioLandscapes.Flamenco_Worker.assets],
@@ -13,5 +14,6 @@ assets = load_assets_from_modules(
 defs = Definitions(
     assets=[
         *assets,
+        *assets_external,
     ],
 )
