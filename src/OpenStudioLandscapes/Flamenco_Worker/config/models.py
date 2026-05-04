aa17c9e194f5facee95bb86f836d4037488741af
Michael Mussato <michimussato@gmail.com>
2026-05-04 14:28:36 +0200

    nox (readmes failed)

--- src/OpenStudioLandscapes/Flamenco_Worker/config/models.py
+++ src/OpenStudioLandscapes/Flamenco_Worker/config/models.py
@@ -1,16 +1,14 @@
 import pathlib
 from typing import List
 
+from OpenStudioLandscapes.engine.config.models import FeatureBaseModel
+from OpenStudioLandscapes.engine.logging.loggers import FEATURE_LOGGER as LOGGER
 from pydantic import (
     Field,
     PositiveInt,
     field_validator,
 )
 from pydantic_core import PydanticCustomError
-
-from OpenStudioLandscapes.engine.logging.loggers import FEATURE_LOGGER as LOGGER
-
-from OpenStudioLandscapes.engine.config.models import FeatureBaseModel
 
 from OpenStudioLandscapes.Flamenco_Worker import constants, dist
 
@@ -72,6 +70,7 @@ if __name__ == "__main__":
     CONFIG_STR = Config.get_docs()
 else:
     import yaml
+
     CONFIG_STR = yaml.dump(
         Config.model_json_schema(mode="serialization"),
     )
